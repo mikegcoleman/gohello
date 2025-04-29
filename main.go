07788b9037d2892d5b4ba21cafba922f05c2a141
Mike Coleman <mikegcoleman@gmail.com>
2025-04-29 15:23:24 -0700

    testing something that already works.

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
-
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hola, Mundo!")
+	fmt.Fprintln(w, "Hola, Mundo!")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    fmt.Println("Server listening on port 80...")
-    if err := http.ListenAndServe(":80", nil); err != nil {
-        fmt.Println("Error starting server:", err)
-    }
+	http.HandleFunc("/", handler)
+	fmt.Println("Server is on port 80...")
+	if err := http.ListenAndServe(":80", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
